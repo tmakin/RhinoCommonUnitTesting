9f8569ec59a7d2101d5af3b7c6b4b19d20035ad8
Marios Tsiliakos <mtsiliak@fosterandpartners.com>
2021-06-17 14:21:23 +0100

    Adds simple Rhino Load project

---
+++ Src/RhinoPlugin.Tests.RHInside/XunitExampleTestsRI.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+using Rhino.Geometry;
+using Grasshopper.Kernel.Types;
+
+using Xunit;
+
+
+namespace RhinoPlugin.Tests.RHInside
+{
+    /// <summary>
+    /// XUnit tests
+    /// </summary>
+    [Collection("RhinoTestingCollection")]
+    public class XunitExampleTestsRI 
+    {
+        XunitTestFixtureRI _fixture;
+
+        public XunitExampleTestsRI(XunitTestFixtureRI fixture)
+        {
+           fixture = _fixture;
+        }
+       
+        [Fact]
+        public void Brep_Translation()
+        {
+
+                // Arrange
+                var bb = new BoundingBox(new Point3d(0, 0, 0), new Point3d(100, 100, 100));
+                var brep = bb.ToBrep();
+                var t = Transform.Translation(new Vector3d(30, 40, 50));
+
+                // Act
+                brep.Transform(t);
+
+                // Assert
+                Assert.Equal(brep.GetBoundingBox(true).Center, new Point3d(80, 90, 100));
+  
+        }
+
+        /// <summary>
+        /// Xunit Test to Intersect sphere with a plane to generate a circle
+        /// </summary>
+        [Fact]
+        public void Brep_Intersection()
+        {
+            // Arrange
+            var radius = 4.0;
+            var brep = Brep.CreateFromSphere(new Sphere(new Point3d(), radius));
+            var cuttingPlane = Plane.WorldXY;
+
+            // Act
+            Rhino.Geometry.Intersect.Intersection.BrepPlane(brep, cuttingPlane, 0.001, out var curves, out var points);
+
+            // Assert
+            Assert.Single(curves);
+            Assert.Equal(2 * Math.PI * radius, curves[0].GetLength());
+        }
+
+        /// <summary>
+        /// Xunit Test to ensure Centroid of GH_Box outputs a GH_Point
+        /// </summary>
+        [Fact]
+        public void GHBox_Centroid_ReturnsGHPoint()
+        {
+            // Arrange
+            var myBox = new GH_Box(new Box());
+
+            // Act 
+            var result = myBox.Boundingbox.Center;
+
+            // Assert
+            Assert.IsType<Point3d>(result);
+        }
+
+    }
+}
---
+++ Src/RhinoPlugin.Tests.RHInside/XunitTestFixtureRI.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace RhinoPlugin.Tests.RHInside
+{
+
+    /// <summary>
+    /// Shared test context across unit tests that loads rhinocommon.dll and grasshopper.dll
+    /// </summary>
+    public class XunitTestFixtureRI : IDisposable
+    {
+ 
+        private Rhino.Runtime.InProcess.RhinoCore _rhinoCore;
+
+        /// <summary>
+        /// Empty Constuctor
+        /// </summary>
+        public XunitTestFixtureRI()
+        {
+            // Use the latest version of RH7 installed in your system
+            RhinoInside.Resolver.UseLatest = true;
+
+            // Make sure we are running the tests as 64x
+            Assert.True(Environment.Is64BitProcess, "Tests must be run as x64");
+
+            //Initialize Rhino.Inside
+            RhinoInside.Resolver.Initialize();
+
+            // Set path to rhino system directory
+            string envPath = Environment.GetEnvironmentVariable("path");
+            Environment.SetEnvironmentVariable("path", envPath + ";" + RhinoInside.Resolver.RhinoSystemDirectory);
+
+            // Start Rhino and load all libraries
+            StartRhino();
+
+        }
+
+        /// <summary>
+        /// Starting Rhino - loading the relevant libraries
+        /// </summary>
+        [STAThread]
+        public void StartRhino()
+        {
+            _rhinoCore = new Rhino.Runtime.InProcess.RhinoCore(null, Rhino.Runtime.InProcess.WindowStyle.NoWindow);
+        }
+
+        /// <summary>
+        /// Disposing the context after running all the tests
+        /// </summary>
+        public void Dispose()
+        {
+            // do nothing or...
+            _rhinoCore?.Dispose();
+            _rhinoCore = null;
+        }
+    }
+
+
+    /// <summary>
+    /// Collection Fixture - shared context across test classes
+    /// </summary>
+    [CollectionDefinition("RhinoTestingCollection")]
+    public class RhinoCollection : ICollectionFixture<XunitTestFixtureRI>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+
+}
+
